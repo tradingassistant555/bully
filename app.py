7d1bec83dcdec5dd1cc18789d05f7fe14f98272a
tradingassistant555 <123533054+tradingassistant555@users.noreply.github.com>
2023-03-16 16:55:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+from keras.models import load_model
+import streamlit as st
+import tiingo
+
+st.title('Stock Trend Prediction MODULE OF BULLS AND BEARS')
+
+user_input=st.text_input('Enter Stock Ticker','AAPL')
+config = {
+    'session': True,
+    'api_key': '69a314c69b5268ab884356e2657b047eec4dc315'
+}
+client = tiingo.TiingoClient(config)
+data = client.get_dataframe(user_input, startDate='2010-01-01', endDate='2023-03-10')
+
+st.subheader(f'Data from 2010-01-01 to 2022-02-28 of {user_input}')
+st.write(data.describe())
+
+st.subheader('Closing Price vs Time chart')
+
+fig=plt.figure(figsize=(12,6))
+plt.plot(data.adjClose)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA')
+ma100=data.adjClose.rolling(100).mean()
+fig=plt.figure(figsize=(12,6))
+plt.plot(data.adjClose)
+plt.plot(ma100)
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100=data.adjClose.rolling(100).mean()
+ma200=data.adjClose.rolling(200).mean()
+fig=plt.figure(figsize=(12,6))
+plt.plot(ma100,'b',label='Original Price')
+plt.plot(ma200,'r',label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.plot(data.adjClose)
+st.pyplot(fig)
+
+data_training=pd.DataFrame(data['adjClose'][0:int(len(data)*0.70)])
+data_testing=pd.DataFrame(data['adjClose'][int(len(data)*0.70):int(len(data))])
+
+
+
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler(feature_range=(0,1))
+
+data_training_array=scaler.fit_transform(data_training)
+
+
+model=load_model('keras_model_h5')
+
+past_100_days=data_training.tail(100)
+final_df=past_100_days.append(data_testing,ignore_index=True)
+input_data=scaler.fit_transform(final_df)
+
+x_test=[]
+y_test=[]
+
+for i in range(100,input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+
+x_test,y_test=np.array(x_test),np.array(y_test) 
+y_predicted=model.predict(x_test)
+
+scaler =scaler.scale_
+
+scale_factor=1/scaler[0]
+y_predicted=y_predicted*scale_factor
+y_test=y_test*scale_factor
+
+st.subheader('Prediction vs Original')
+
+fig2=plt.figure(figsize=(12,6))
+plt.plot(y_test,'b',label='Original Price')
+plt.plot(y_predicted,'r',label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+st.pyplot(fig2)
